afe843b60bba0be931377488e4bb1d709a488cd3
David Uzor <duzor144@gmail.com>
2024-03-31 23:56:27 -0400

    Implemented methods for move left, right, up and down.
    implemented gameover method

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* Code below initializes the game
+/* Code below initializes the array used for the game
 */
 let array = [ 
     [0,0,0,0],
@@ -7,45 +7,90 @@ let array = [
     [0,0,0,0]   
 ];
 
-//test
-//array[0][1] = 2;
-//array[0][3] = 2; 
+const gameOver = (array) => {
+    for (let r = 1; r < array.length-1; r++) {
+        let prev = r-1;
+        let next = r+1;
+        for (let c = 0; c < array.length; c++) {
+            if (array[prev][c] === array[r][c] || array[r][c] === array[next][c] ) {
+                return false;
+            }            
+        }
+        
+    }
+    
+    for (let c = 1; c < array.length-1; c++) {
+        let prev = c-1;
+        let next = c+1;
+        for (let r = 0; r < array.length; r++) {
+            if (array[r][prev] === array[r][c] || array[r][c] === array[r][next]) {
+                return false;
+            }
+        }
+    }
 
-/*
-//-- adding 2's in random places
-let randRow = Math.floor(Math.random()*4);
-let randCol = Math.floor(Math.random()*4);
+    return true;
+}
+const addRandomTwoToGrid = (array) => {
+    let randRow = Math.floor(Math.random()*4);
+    let randCol = Math.floor(Math.random()*4);
 
-array[randRow][randCol] = 2;
+    while (array[randRow][randCol] !== 0) {
+        randRow = Math.floor(Math.random()*4);
+        randCol = Math.floor(Math.random()*4);
+    }
 
-let randRow2 = randRow;
-let randCol2 = randCol;
-
-while (randRow2 === randRow || randCol2 === randCol) {
-    randRow2 = Math.floor(Math.random()*4);
-    randCol2 = Math.floor(Math.random()*4);
+    array[randRow][randCol] = 2;  
 }
 
-array[randRow2][randCol2] = 2;
-//-- adding 2's in random places
-*/
+const addRandomTwosToGrid = (array) => {
+    let randRow = Math.floor(Math.random()*4);
+    let randCol = Math.floor(Math.random()*4);
+
+    while (array[randRow][randCol] !== 0) {
+        randRow = Math.floor(Math.random()*4);
+        randCol = Math.floor(Math.random()*4);
+    }
+
+    array[randRow][randCol] = 2;
+
+    let randRow2 = randRow;
+    let randCol2 = randCol;
+
+    while (array[randRow2][randCol2] !== 0) {
+        randRow2 = Math.floor(Math.random()*4);
+        randCol2 = Math.floor(Math.random()*4);
+    }
+
+    array[randRow2][randCol2] = 2;
+    
+}
+
+addRandomTwosToGrid(array);
+
+
 
 
 // --- printing array
 
-// --- for testing
+/* --- for testing
 array[0][0] = 2;
 array[0][1] = 2;
 array[0][2] = 2;
 array[0][3] = 2;
 array[1][0] = 2;
-array[1][1] = 0;
+array[1][1] = 2;
 array[1][2] = 2;
-array[1][3] = 0;
+array[1][3] = 2;
+array[2][2] = 2;
+array[3][0] = 2;
+array[3][3] = 4;
+*/
+
 for (var i = 0; i < array.length; i++) {
     console.log(array[i].join(" "));
 }
-// --- for testing
+
 
 // reset function prepares grid for new game
 const reset = () => {
@@ -73,35 +118,115 @@ const reset = () => {
 
 } //reset
 
+
+
+/* Shifts numbers in grid from 
+ * bottom to top.
+*/
 const moveDown = () => {
+    let merged = Array.from({ length: array.length }, () => Array(array.length).fill(false));
+    for (let c = 0; c < array.length; c++) {
+        for (let r = array.length - 1; r >= 0; r--) {
+            for (let i = r - 1; i >= 0; i--) {
+                if (array[i][c] === 0) {
+                    continue;
+                }
+                if (array[r][c] === array[i][c] && !merged[r][c] && !merged[i][c] && (array[i][c] === array[r-1][c] || (array[i][c] === array[r-2][c] && array[r-1][c] === 0) || (array[i][c] === array[r-3][c] && array[r-1][c] === 0 && array[r-2][c] === 0))) {
+                    array[r][c] = array[r][c] * 2;
+                    array[i][c] = 0;
+                    merged[r][c] = true;
+                } else if (array[r-1][c] === 0 && array[r][c] !== 0) {
+                    array[r-1][c] = array[i][c];
+                    array[i][c] = 0;
+                    break;
+                } else if (array[r][c] === 0) {
+                    array[r][c] = array[i][c];
+                    array[i][c] = 0;
+                }
+            }
+        }
+    }
+}
 
-} // moveDown
-
+/* Shifts numbers in grid from 
+ * bottom to top.
+*/
 const moveUp = () => {
+    let merged = Array.from({ length: array.length }, () => Array(array.length).fill(false));
+    for (let c = 0; c < array.length; c++) {
+        for (let r = 0; r < array.length; r++) {
+            for (let i = r + 1; i < array.length; i++) {
+                if (array[i][c] === 0) {
+                    continue;
+                }
+                if (array[r][c] === array[i][c] && !merged[r][c] && !merged[i][c] && (array[i][c] === array[r+1][c] || (array[i][c] === array[r+2][c] && array[r+1][c] === 0) || (array[i][c] === array[r+3][c] && array[r+1][c] === 0 && array[r+2][c] === 0))) {
+                    array[r][c] = array[r][c] * 2;
+                    array[i][c] = 0;
+                    merged[r][c] = true;
+                } else if (array[r+1][c] === 0 && array[r][c] !== 0) {
+                    array[r+1][c] = array[i][c];
+                    array[i][c] = 0;
+                    break;
+                } else if (array[r][c] === 0) {
+                    array[r][c] = array[i][c];
+                    array[i][c] = 0;
+                }
+            }
+        }
+    }    
 } // moveUp
 
+/* Shifts numbers in grid from 
+ * left to right.
+*/
 const moveRight = () => {
-
+    let merged = Array.from({ length: array.length }, () => Array(array.length).fill(false));
+    for (let r = 0; r < array.length; r++) {
+        for (let c = array.length-1; c >= 0; c--) {
+            for (let j = c-1; j >= 0; j--) {
+                if (array[r][j] === 0) {
+                    continue;
+                }
+                if (array[r][c] === array[r][j] && !merged[r][c] && !merged[r][j] && (array[r][j] === array[r][c-1] || (array[r][j] === array[r][c-2] && array[r][c-1] === 0) || (array[r][j] === array[r][c-3] && array[r][c-1] === 0 && array[r][c-2] === 0))) {
+                    array[r][c] = array[r][c]*2;
+                    array[r][j] = 0;
+                    merged[r][c] = true;
+                } else if (array[r][c-1] === 0 && array[r][c] !== 0) {
+                    array[r][c-1] = array[r][j];
+                    array[r][j] = 0;
+                    break;
+                } else if (array[r][c] === 0) {
+                    array[r][c] = array[r][j];
+                    array[r][j] = 0;
+                }
+            }
+        }
+    }
 } // moveRight
 
-//supposed to move numbers in a row to the left, if 2 numbers with 0's
-// in between them, or consecutive, the are sdded snd put in the closest position they cane be to the left.
-const moveLeft = () => { 
+/* Shifts numbers in grid from 
+ * right to left.
+*/
+const moveLeft = () => {
+    let merged = Array.from({ length: array.length }, () => Array(array.length).fill(false));
     for (let r = 0; r < array.length; r++) {
         for (let c = 0; c < array.length; c++) {
             for (let j = c +1; j < array.length; j++) {
                 if (array[r][j] === 0) {
                     continue;
                 }
-                if (array[r][c] === array[r][j]) {
+                if (array[r][c] === array[r][j] && !merged[r][c] && !merged[r][j] && (array[r][j] === array[r][c+1] || (array[r][j] === array[r][c+2] && array[r][c+1] === 0) || (array[r][j] === array[r][c+3] && array[r][c+1] === 0 && array[r][c+2] === 0) )) {
                     array[r][c] = array[r][c]*2;
                     array[r][j] = 0;
-                } else if (array[r][c+1] === 0) {
+                    merged[r][c] = true;
+                } else if (array[r][c+1] === 0 && array[r][c] !== 0) {
                     array[r][c+1] = array[r][j];
                     array[r][j] = 0;
-                    
+                    break;              
+                } else if (array[r][c] === 0) {
+                    array[r][c] = array[r][j];
+                    array[r][j] = 0; 
                 }
-
             }
         }
     }
@@ -129,6 +254,7 @@ const keyPress = (event) => {
     }
 
     if (arrowKeyPressed) {
+        addRandomTwoToGrid(array);
         // --- printing array
         for (var i = 0; i < array.length; i++) {
             console.log(array[i].join(" "));
@@ -141,5 +267,6 @@ const keyPress = (event) => {
 
 
 
-
-document.addEventListener('keydown',keyPress);
+if (!gameOver(array)) {
+    document.addEventListener('keydown',keyPress);
+}
